39ed1a1dca0d197308446bab39d0c6849806e4cc
Daniele Tirinnanzi <danieletiri@gmail.com>
2023-08-29 10:27:54 +0200

    re-generated degree distribution images (N=300)

--- degree distribution matching/N300/cliqueless graphs/comparison_matplotlib_cliqueLessGraphs.py
+++ degree distribution matching/N300/cliqueless graphs/comparison_matplotlib_cliqueLessGraphs.py
@@ -1,25 +1,32 @@
 import json
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 # importing averageDegreeObject.json
-with open('./currentComparisonObject_cliqueLessGraphs.json') as current_comparison_file:
+with open('degree distribution matching/N300/cliqueless graphs/currentComparisonObject_cliqueLessGraphs.json') as current_comparison_file:
     current_comparison = json.load(current_comparison_file)
+
+fig, ax = plt.subplots(figsize=(6,4))
 
 # Extract the data that has to be plotted
 x = current_comparison["arrayOfCliqueSizes"]
 y_uncorrected = current_comparison["uncorrectedGraphsAverageDegreeArray"]
 y_corrected = current_comparison["correctedGraphsAverageDegreeArray"]
 number_of_nodes = current_comparison["numberOfNodes"]
-title_string = f"Average degree distribution for graphs of {number_of_nodes} nodes."
+title_string = f"Average degree distribution for graphs of {number_of_nodes} nodes without clique"
+
+# create dataframe with x, y_corrected and y_uncorrected as columns:
+df = pd.DataFrame({'x': x, 'y_corrected': y_corrected, 'y_uncorrected': y_uncorrected})
 
 # Plot the data
-plt.plot(x, y_uncorrected, label="Uncorrected graphs")
-plt.plot(x, y_corrected, label="Corrected graphs")
-plt.xlabel("Clique Size")
-plt.ylabel("Average Degree")
-plt.title(title_string)
-plt.legend()
-plt.show()
+ax = sns.lineplot(data=df, x="x", y="y_corrected", label="Corrected graphs", color='blue')
+ax = sns.lineplot(data=df, x="x", y="y_uncorrected", label="Uncorrected graphs", color = 'green')
+ax.set_ylabel("Average Degree")
+ax.set_xlabel("Clique Size")
+ax.set_title(title_string)
+ax.legend()
+fig.show()
 
-# # Save plot as high resolution png:
-# plt.savefig('./images/N300_cliqueLessGraphs.png', dpi=300)
+# Save plot as high resolution png:
+fig.savefig('degree distribution matching/N300/cliqueless graphs/images/N300_cliqueLessGraphs.png', dpi=300)
--- degree distribution matching/N300/couple of graphs/comparison_matplotlib_coupleOfGraphs.py
+++ degree distribution matching/N300/couple of graphs/comparison_matplotlib_coupleOfGraphs.py
@@ -1,22 +1,33 @@
 import json
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 # importing averageDegreeObject.json
-with open('degree distribution matching/couple of graphs/currentComparisonObject_coupleOfGraphs.json') as current_comparison_file:
-  current_comparison = json.load(current_comparison_file)
+with open('degree distribution matching/N300/couple of graphs/currentComparisonObject_coupleOfGraphs.json') as current_comparison_file:
+    current_comparison = json.load(current_comparison_file)
+
+fig, ax = plt.subplots(figsize=(6,4))
 
 # Extract the data that has to be plotted
 x = current_comparison["arrayOfCliqueSizes"]
 y_with_clique = current_comparison["graphsWithCliqueAverageDegreeArray"]
 y_without_clique = current_comparison["graphsWithoutCliqueAverageDegreeArray"]
 number_of_nodes = current_comparison["numberOfNodes"]
-title_string = f"Average degree distribution for graphs of {number_of_nodes} nodes."
+title_string = f"Average degree distribution for couple of graphs of {number_of_nodes} nodes"
+
+
+# create dataframe with x, y_corrected and y_uncorrected as columns:
+df = pd.DataFrame({'x': x, 'y_with_clique': y_with_clique, 'y_without_clique': y_without_clique})
 
 # Plot the data
-plt.plot(x, y_with_clique, label="Graphs with clique")
-plt.plot(x, y_without_clique, label="Graphs without clique (corrected)")
-plt.xlabel("Clique Size")
-plt.ylabel("Average Degree")
-plt.title(title_string)
-plt.legend()
-plt.show()
+ax = sns.lineplot(data=df, x="x", y="y_without_clique", label="Graphs without clique (corrected)", color='blue')
+ax = sns.lineplot(data=df, x="x", y="y_with_clique", label="Graphs with clique", color='red')
+ax.set_ylabel("Average Degree")
+ax.set_xlabel("Clique Size")
+ax.legend()
+ax.set_title(title_string)
+fig.show()
+
+# Save plot as high resolution png:
+fig.savefig('degree distribution matching/N300/couple of graphs/images/N300_coupleOfGraphs.png', dpi=300)
